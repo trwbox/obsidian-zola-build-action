8b75fe10651f163ada73409e5a038b6cdb396972
trwbox <40971149+trwbox@users.noreply.github.com>
2022-07-06 21:22:47 -0500

    Update entrypoint.sh
    Cleanup and attempt v1.3.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,19 +49,23 @@ main() {
     echo "Starting deploy..."
 
     git config --global url."https://".insteadOf git://
-    ## $GITHUB_SERVER_URL is set as a default environment variable in all workflows, default is https://github.com
     git config --global url."$GITHUB_SERVER_URL/".insteadOf "git@${GITHUB_HOSTNAME}":
     if [[ "$BUILD_THEMES" ]]; then
         echo "Fetching themes"
         git submodule update --init --recursive
     fi
-
+    
+    # Printing out the zola version
     version=$(zola --version)
-    mkdir public
     echo "Version: $version"
+    
+    # Set the remote repo
     remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@${GITHUB_HOSTNAME}/${TARGET_REPOSITORY}.git"
     remote_branch=$PAGES_BRANCH
-
+    
+    # Making the needed directories
+    echo "Making public directory"
+    mkdir public
     echo "Making __obsidian directory"
     mkdir __obsidian
     # This will throw a subdirectory error, that is okay
@@ -69,16 +73,14 @@ main() {
     echo "If there is a subdirectory error that is fine"
         
     # Clone the main repo at a specific version
-    echo "Using obsidian-zola version: v1.3.0"
-    git clone https://github.com/ppeetteerrs/obsidian-zola.git --branch v1.3.0 __site
+    echo "Using obsidian-zola version: v1.3.1"
+    git clone https://github.com/ppeetteerrs/obsidian-zola.git --branch v1.3.1 __site
     # Move the netlify.toml into that directory
     if [ ! -f __obsidian/netlify.toml ]; then
     	echo "No netlify.toml. Exiting"
 	exit 1
     fi
     echo "Found netlify.toml"
-    #echo "Moving netlify.toml"
-    #mv __obsidian/netlify.toml __site/
     
     export VAULT="../__obsidian"
     echo "Vault path: $VAULT"
@@ -95,30 +97,35 @@ main() {
     rsync -a __site/zola/ __site/build
     echo "Moving content to content"
     rsync -a __site/content/ __site/build/content   
-    
     mkdir -p __site/build/content/docs __site/build/__docs
+    
+    # Use obsidian-export
     if [ -z "$STRICT_LINE_BREAKS" ]; then
 	    __site/bin/obsidian-export --frontmatter=never --hard-linebreaks --no-recursive-embeds __obsidian __site/build/__docs
     else
 	    __site/bin/obsidian-export --frontmatter=never --no-recursive-embeds __obsidian __site/build/__docs
     fi
     
+    # Run the conversion script
     python __site/convert.py
     
+    # Build the site
     zola --root __site/build build --output-dir public
 
+    # If there was a CNAME file for a custom subdomain, copy it
     if [ -f __obsidian/CNAME ]; then
         echo "Found a CNAME record copying"
 	cp  __obsidian/CNAME public/
     fi
 
+    # If set to build only as a non-deployment test stop, otherwise push to gh-pages branch
     if ${BUILD_ONLY}; then
         echo "Build complete. Deployment skipped by request"
         exit 0
     else
         echo "Pushing artifacts to ${TARGET_REPOSITORY}:$remote_branch"
 
-	echo "Going into build branch?"
+	echo "Going into build directory"
         cd public
         git init
         git branch -m main
@@ -127,7 +134,6 @@ main() {
         git add .
 
         git commit -m "Deploy ${TARGET_REPOSITORY} to ${TARGET_REPOSITORY}:$remote_branch"
-        echo "Trying main instead of master"
         git push --force "${remote_repo}" main:${remote_branch}
 
         echo "Deploy complete"
