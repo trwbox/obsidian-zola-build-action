2d9c6e0d592ee78bc2b8b7614da539fd448ca699
trwbox <40971149+trwbox@users.noreply.github.com>
2022-08-01 21:21:09 -0500

    Update entrypoint.sh
    Bash is hard sometimes

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,7 +79,7 @@ main() {
     # If there is a ZOLA_REPO enviroment variable specified by the user clone that
     if [[ -n "$ZOLA_REPO" ]]; then 
         echo "Cloning from the user specified obsidian-zola repo"
-        git clone $ZOLE_REPO __site
+        git clone ${ZOLE_REPO} __site
     else
         # Clone the main repo at a specific version
         echo "Using obsidian-zola version: v1.3.1"
